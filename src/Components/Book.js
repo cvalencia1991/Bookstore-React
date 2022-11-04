54622f25786266bf6076073d3bd0960a2ba3d3de
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-04 06:09:02 -0500

    Add props to the project

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,24 +1,24 @@
 import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
 
-const books = [{
-  id: 1,
-  autor: ' Gabriel garcia marquez',
-  book: ' Cien años de soledad',
-},
-];
-const Book = () => (
-  <div className="d-flex align-items-center justify-content-center gap-3 m-3">
-    <ul className="m-0 p-0 stylebook">
-      {books.map((Booklib) => (
-        <li key={Booklib.id}>
-          {Booklib.autor}
+const Book = (props) => {
+  Book.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  };
+  const { title, author } = props;
+  return (
+    <div className="d-flex align-items-center justify-content-center gap-3 m-3">
+      <ul className="m-0 p-0 stylebook">
+        <li>
+          {title}
           :
-          {' '}
-          {Booklib.book}
+          {author}
         </li>
-      ))}
-    </ul>
-    <Button>Remove</Button>
-  </div>
-);
+      </ul>
+      <Button>Remove</Button>
+    </div>
+  );
+};
+
 export default Book;
