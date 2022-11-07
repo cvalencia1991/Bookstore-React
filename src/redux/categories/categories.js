68b29d8e17f55ee47598ed76676de4a21920b4c4
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-07 17:40:23 -0500

    Create books and categories

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,13 +1,6 @@
-/* Define an action type for checking the status.
-Set the initial state to be an empty array of categories.
-Export Action Creators for your actions.
-Write your reducer and export it as default.
-For the check status action return a string "Under construction".
-In case of unknown action - return the current state. */
-
 const CHECKING = 'CHECK';
 export const checking = () => ({ type: CHECKING });
-export default function Categories(state = [], action) {
+export default function CheckingReducer(state = [], action) {
   switch (action.type) {
     case CHECKING:
       return 'Under construction';
